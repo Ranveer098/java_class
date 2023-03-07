ffd34adb30ea68ce74e463f5d55091e6f288bf41
Nageshwar Yadav <95100082+Ranveer098@users.noreply.github.com>
2023-03-07 13:00:01 +0530

    init

--- Assignments/Assg-2/code1.java
+++ Assignments/Assg-2/code1.java
@@ -1,34 +1,50 @@
-//Write a Java program to get the difference between the largest and smallest values in an array of integers.
 import java.util.*;
-import java.lang.Math;
+import java.text.*;
 
-public class code1{
-     public static void main(String[] args)
-     {
-          int n;
-          Scanner scan=new Scanner(System.in);
-          System.out.print("Size of array = ");
-          n=scan.nextInt();
-
-          int[] arr;
-          arr=new int[n];
-          int min=Integer.MAX_VALUE;
-          int max=Integer.MIN_VALUE;
-          System.out.print("Enter array elements: ");
-
-          for (int i=0; i<n; i++)
-          {
-               arr[i]=scan.nextInt();
-               if (arr[i]>max)
-                    max=arr[i];
-               if (arr[i]<min)
-                    min=arr[i];
-          }
-
-          System.out.println("Smallest = "+min);
-          System.out.println("Largest = "+max);
-          System.out.println("Difference = "+Math.abs(max-min));
-
-          scan.close();
-     }
+class ItemType{
+    public String name;
+    public double deposit;
+    public double costPerDay;
+    public ItemType()
+    {
+        this.name=null;
+        this.deposit=0;
+        this.costPerDay=0;
+    }
+    public ItemType(String name, Double deposit, Double costPerDay)
+    {
+        this.name=name;
+        this.deposit=deposit;
+        this.costPerDay=costPerDay;
+        System.out.println(this.name);
+        System.out.println(this.deposit);
+        System.out.println(this.costPerDay);
+    }
+}
+class ItemTypeBO{
+    public Double calculateCost(Date start, Date end, ItemType typeIns)
+    {
+        long diff=(start.getTime()-end.getTime())/86400000;
+        double result=diff*typeIns.costPerDay;
+        return result;
+    }
+}
+class Main{
+    public static void main(String[] args) throws ParseException{
+        ItemType i=new ItemType();
+        Scanner scan=new Scanner(System.in);
+        DecimalFormat dd=new DecimalFormat("0.0");
+        i.name=scan.nextLine();
+        i.deposit=Double.parseDouble(scan.nextLine());
+        i.costPerDay=Double.parseDouble(scan.nextLine());
+        String date1=scan.nextLine();
+        String date2=scan.nextLine();
+        ItemType i1=new ItemType(i.name, i.deposit, i.costPerDay);
+        Date start=new SimpleDateFormat("dd/MM/yyyy").parse(date1);
+        Date end=new SimpleDateFormat("dd/MM/yyyy").parse(date2);
+        ItemTypeBO iBO=new ItemTypeBO();
+        double result=iBO.calculateCost(start, end, i1);
+        System.out.println(dd.format(Math.abs(result)));
+        scan.close();
+    }
 }
